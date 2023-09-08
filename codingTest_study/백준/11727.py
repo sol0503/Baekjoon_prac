2250ad7ba3dfafc847aa2b6fe3e154cfbac3ad56
sol0503 <jbkljo@naver.com>
2023-09-08 21:32:31 +0900

    feat: 11727 다시올리기

--- codingTest_study/백준/11727.py
+++ codingTest_study/백준/11727.py
@@ -4,8 +4,11 @@ input = sys.stdin.readline
 n = int(input())
 
 dp = [0]*(n+1)
-dp[1] = 1
-dp[2] = 3
+if n >= 1:
+    dp[1] = 1
+if n >= 2:
+    dp[2] = 3
+
 for i in range(3, n+1):
     dp[i] = 2*dp[i-2]+dp[i-1]
 
